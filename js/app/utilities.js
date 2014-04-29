c4c64d654e97c81f16e1cc6fa957bda8c288a958
amiceli <antoine_miceli@natural-solutions.eu>
2014-04-29 11:14:46 +0200

    Create utilities file form common function

---
+++ js/app/utilities.js
@@ -0,0 +1,111 @@
+var formBuilder = (function(formBuild) {
+
+    /**
+     * Return if the XML Content is valid
+     * 
+     * @param {string} xmlContent xml content to check
+     * @returns {array|Boolean} a boolean if everything is ok or an array with the errors
+     */
+    formBuild.XMLValidation = function(xmlContent) {
+            
+            $.ajax({                
+                url         : 'xml/NS_Schema.xsd',
+                dataType    : 'html',
+                async       : false                
+            }).done(function(data) {
+                
+                var Module = {
+                    xml         : xmlContent,
+                    schema      : data,
+                    arguments   : ["--noout", "--schema", 'NS_Schema.xsd', 'output.xml']
+                }, xmllint = validateXML(Module);
+
+                if (xmllint.indexOf('validates') > 0) {
+                    result = true;
+                } else {
+                    var split   = xmllint.split(':');
+                    result      = {
+                        error   : split[3],
+                        element : split[2],
+                        message : split[split.length - 1].split('.', 1)[0]
+                    };
+                }
+
+            }).error(function(jqXHR, textStatus, errorThrown) {
+                throw errorThrown.message;
+            });
+
+            return result;
+    };
+    
+    /**
+     * 
+     * @param {type} baseTText
+     * @param {type} newText
+     * @param {type} selector
+     * @param {type} contextSize
+     * @param {type} baseTextName
+     * @param {type} newTextName
+     * @param {type} inline
+     * @returns {diffview.buildView.ctelt.e|Element|diffview.buildView.celt.e}
+     */
+    formBuild.GetXMLDiff = function (baseText, newText, baseTextName, newTextName, inline, contextSize) {
+        
+        var base    = difflib.stringAsLines(baseText), 
+            newtxt  = difflib.stringAsLines(newText);
+        var sm      = new difflib.SequenceMatcher(base, newtxt);
+
+        var opcodes         = sm.get_opcodes();
+            /*diffoutputdiv   = $(selector);
+        
+        while (diffoutputdiv.firstChild) {
+            diffoutputdiv.removeChild(diffoutputdiv.firstChild);
+        }*/
+
+        // build the diff view and add it to the current DOM
+        return diffview.buildView({
+            baseTextLines   : base,
+            newTextLines    : newtxt,
+            opcodes         : opcodes,
+
+            // set the display titles for each resource
+
+            baseTextName    : baseTextName  || 'Source file',
+            newTextName     : newTextName   || 'Updated file',
+            contextSize     : contextSize   || null,
+            viewType        : inline        ||  0
+        });
+    };
+    
+    /**
+     * 
+     * @param {type} title
+     * @param {type} msg
+     * @returns {undefined}
+     */
+    formBuild.displayError = function(title, msg) {
+        var modal = $(
+                        '<div id="dialog-message" title="' + title + '">' +
+                            '<p>' +
+                                '<span class="ui-icon ui-icon-alert" style="float:left; margin:0 7px 50px 0;"></span>' +
+                                msg +
+                            '</p>' +
+                        '</div>'
+                    );
+        $(modal).dialog({
+            modal       : true,
+            width       : 500,
+            height      : 250,
+            position    : 'center',
+            draggable   : false,
+            buttons: {
+                Ok: function() {
+                    $(this).dialog("close");
+                }
+            }
+        });
+    };
+    
+    return formBuild;
+
+})(formBuilder);
