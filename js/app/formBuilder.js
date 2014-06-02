eddc99204a9f9727a0290d1a4758fdfcc3e16b3e
amiceli <antoine_miceli@natural-solutions.eu>
2014-06-02 14:44:38 +0200

    Add main view

--- js/app/formBuilder.js
+++ js/app/formBuilder.js
@@ -11,41 +11,45 @@
  */
 var formBuilder = (function(formBuild) {
 
-    formBuild = {
-        init: function() {
-                $("#formBuilder").html(
-                    '<div class="row-fluid content">' +
-                        '<div class="span3 widgetsPanel nano"></div>' +
-                        '<div class="span9 dropArea"></div>' +
-                        '<div class="settings span5"></div>' +
-                    '</div>'
-                );
+    //  Fix me
+    formBuild = {};
+    
+    formBuild.MainView = Backbone.View.extend({
+        initialize : function(options) {
+            
+            $(this.el).append(
+                '<div class="row-fluid content">'+
+                '   <div class="span3 widgetsPanel nano"></div>'+
+                '   <div class="span9 dropArea"></div>'+
+                '   <div class="settings span5"></div>'+
+                '</div>'
+            );
+            this.form = new formBuild.Form({}, {
+                name: "My form"
+            });
 
-                this.form = new formBuild.Form({}, {
-                    name: "My form"
-                });
+            this.panelView = new formBuild.PanelView({
+                el: $('.widgetsPanel'),
+                collection: this.form,
+            });
 
-                this.panelView = new formBuild.PanelView({
-                    el: $('.widgetsPanel'),
-                    collection: this.form,
-                });
-                this.panelView.render();
-
-                this.formView = new formBuild.FormView({
-                    collection: this.form,
-                    el: $('.dropArea')
-                });
-                this.formView.render();
-
-                this.settingsView = new formBuild.SettingView({
-                    el: $('.settings')
-                });
-                this.settingsView.render();
+            this.formView = new formBuild.FormView({
+                collection: this.form,
+                el: $('.dropArea')
+            });
+            
+            /*this.settingsView = new formBuild.SettingView({
+                el: $('.settings')
+            });*/
+            
+            this.panelView.render();
+            this.formView.render();
+            //this.settingsView.render();
         },
-        clear : function() {
+        clear: function() {
             this.form.clearAll();
-        }
-    };
+        },
+    });   
 
     return formBuild;
 
