e6f60fc4e503208c1a4014a6142e61a6ab16711d
amiceli <antoine_miceli@natural-solutions.eu>
2014-07-07 14:43:28 +0200

    Remove brain file with Backbon router implementation

---
+++ js/router.js
@@ -0,0 +1,41 @@
+var formBuilder = (function(app) {
+    
+    app.Router = Backbone.Router.extend({
+        
+        routes : {
+            "" : 'home',
+            'saveprotocol' : 'saveProtocol',
+            'setting/:id' : 'modelSetting'
+        },
+        
+        home : function() {            
+            if (app.instances.mainView  === undefined || app.instances.currentForm === undefined) {
+                app.instances.currentForm = new app.collections.Form( {}, { name: "My protocol" });
+
+                app.instances.mainView = new app.views.MainView({
+                    el      : '#formBuilder',
+                    form    : app.instances.currentForm
+                });
+            }
+            
+            app.instances.navbar.setActions (app.instances.mainView.getActions());
+            
+        },
+        
+        saveProtocol : function (options) {
+        },
+        
+        modelSetting : function(modelID) {
+            var edit = new app.views.BaseEditView({
+                el: $('.settings'),
+                model : app.instances.currentForm.models[modelID]
+            });
+            edit.render();
+            app.instances.navbar.setActions (edit.getActions());
+        }
+        
+    });
+    
+    return app;
+    
+})(formBuilder);
