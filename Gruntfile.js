2f0a9143284f1ff110d956958b0202251630317e
amiceli <antoine_miceli@natural-solutions.eu>
2014-08-05 16:53:08 +0200

    Update Grunt implementation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,9 @@
 module.exports = function(grunt) {
 
-  // Configuration de Grunt
     grunt.initConfig({
-        
+        //  --------------------------------------------
+        //  Compile LESS files
+        //  --------------------------------------------
         less: {
             dist: {
                 options: {
@@ -13,56 +14,62 @@ module.exports = function(grunt) {
                 }
             }
         },
-        
+
+        //  --------------------------------------------
+        //  Concat all js files (not librairies files)
+        //  FIX Me : outputed file not works !
+        //  --------------------------------------------
         concat: {
             dist: {
-                src: 'js/app/*.js',
+                src: [
+                    'js/formBuilder.js',
+                    'js/utilities.js',
+                    'js/model.js',
+                    'js/collection.js',
+                    'js/views.js',
+                    'js/router.js',
+                    'js/brain.js',
+                ],
                 dest: 'dist/js/formbuilder.js'
-            },
-            libs : {
-                src : 'js/libs/**/*.js',
-                dest: 'dist/js/libraries.js'
             }
         },
-        
+
+        //  --------------------------------------------
+        //  Minify concated js file
+        //  --------------------------------------------
         uglify: {
             dist: {
                 src: 'dist/js/formbuilder.js',
                 dest: 'dist/js/formbuilder.min.js'
             },
         },
-        
+
+        //  --------------------------------------------
+        //  Minify CSS file
+        //  --------------------------------------------
         cssmin : {
             dist : {
                 src: 'dist/stylesheet/formbuilder.css',
                 dest: 'dist/stylesheet/formbuilder.min.css'
             }
         },
-        
+
+        //  --------------------------------------------
+        //  Watch file event and run task
+        //  --------------------------------------------
         watch: {
-            scripts: {
-                files: '**/*.js', // tous les fichiers JavaScript de n'importe quel dossier
-                tasks: ['concat:dist']
-            },
-            styles: {
-                files: '**/*.less', // tous les fichiers Sass de n'importe quel dossier
-                tasks: ['less:dist']
+            stylesheet : {
+                files : ['stylesheet/*.less'],
+                tasks : ['less:dist', 'cssmin:dist']
             }
         }
-    })
+    });
 
-    grunt.loadNpmTasks('grunt-contrib-less')
-    grunt.loadNpmTasks('grunt-contrib-concat')
-    grunt.loadNpmTasks('grunt-contrib-uglify')
-    grunt.loadNpmTasks('grunt-contrib-cssmin')
-    grunt.loadNpmTasks('grunt-contrib-watch')
-    
-    
-    // Définition des tâches Grunt
-    
-    //grunt.registerTask('default', ['dev', 'watch'])
-    
-    grunt.registerTask('dev', ['less', 'concat', 'uglify', 'cssmin'])
-    //grunt.registerTask('dist', ['less:dist', 'uglify:dist'])
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
+    grunt.registerTask('dev', ['less', 'concat', 'uglify', 'cssmin']);
 }
