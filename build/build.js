8def1830119c8f8566a991d444c942a51a4a943b
amiceli <miceli.antoine@gmail.com>
2014-09-22 16:50:56 +0200

    Add r.js configuration file and exemple build file

---
+++ build/build.js
@@ -0,0 +1,50 @@
+({
+	paths: {
+        backbone             : "../../libs/backbone/backbone",
+        blobjs               : "../../libs/blobjs/Blob",
+        bootstrap            : "../../libs/bootstrap/dist/js/bootstrap.min",
+        fancytree            : "../../libs/fancytree/jquery.fancytree-custom.min",
+        filesaver            : "../../libs/filesaver/FileSaver",
+        i18n                 : "../../libs/i18n/i18next",
+        jquery               : "../../libs/jquery/dist/jquery.min",
+        jqueryui             : '../../libs/jquery-ui/jquery-ui',
+        nanoscroller         : "../../libs/nanoscroller/jquery.nanoscroller",
+        underscore           : "../../libs/underscore/underscore",
+        "NS.UI.Navbar"       : "../../libs/NS.UI.Navbar/navbar",
+        "NS.UI.Notification" : "../../libs/NS.UI.Notification/notification",
+        requirejs            : "../../libs/requirejs/require"
+    },
+
+    shim: {
+        'jquery'       : { exports: '$' },
+        'underscore'   : { exports: '_' },
+        'backbone'     : { deps : ['underscore', 'jquery'], exports : "Backbone"},
+        "jqueryui"     : { exports: "$", deps: ['jquery'] },
+        "i18n"         : { exports: "$", deps: ['jquery'] },
+        "nanoscroller" : { exports: "$", deps: ['jquery', 'jqueryui'] },
+        "NS.UI.Navbar" : { exports: "$", deps: ['jquery', 'backbone', 'bootstrap'] },
+        "bootstrap"    : { exports: "$", deps: ['jquery'] },
+    },
+
+    optimize: 'uglify2',
+uglify2: {
+  output: {
+    beautify: true,
+  },
+  beautify: {
+    semicolons: false
+  }
+},
+
+	baseUrl                 : '../assets/js/',
+	mainConfigFile          : '../assets/js/config.js',
+	name                    : 'formbuilder',
+	out                     : 'formbuilder.min.js',
+    output : {
+        beautify: true
+    },
+	preserveLicenseComments : false,
+	
+
+    include : ['requirejs', 'backbone'],
+})
