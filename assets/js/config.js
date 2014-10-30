1fa66c3dc663d13ae4db66570e5d675d67c3e169
amiceli <miceli.antoine@gmail.com>
2014-10-30 16:36:38 +0100

    add references for jquery simple slider library

--- assets/js/config.js
+++ assets/js/config.js
@@ -9,55 +9,144 @@
 
 require.config({
     paths: {
-        backbone               : "../../libs/backbone/backbone",
-        blobjs                 : "../../libs/blobjs/Blob",
-        bootstrap              : "../../libs/bootstrap/dist/js/bootstrap",
-        fancytree              : "../../libs/fancytree/dist/jquery.fancytree-all.min",
-        filesaver              : "../../libs/filesaver/FileSaver",
-        i18n                   : "../../libs/i18n/i18next",
-        jquery                 : "../../libs/jquery/dist/jquery",
-        'jquery-ui'            : "../../libs/jquery-ui/jquery-ui",
-        underscore             : "../../libs/underscore/underscore",
-        "NS.UI.Navbar"         : "../../libs/NS.UI.Navbar/navbar",
-        "NS.UI.NavbarTheme"    : "../../libs/NS.UI.Navbar/themes/navbar.bootstrap3",
-        "NS.UI.Notification"   : "../../libs/NS.UI.Notification/notification",
-        requirejs              : "../../libs/requirejs/require",
-        "font-awesome"         : "../../libs/font-awesome/fonts/*",
-        "backbone-forms"       : "../../libs/backbone-forms/distribution.amd/backbone-forms.min",
-        modalAdapter           : "../../libs/bootstrapAdapter/src/backbone.bootstrap-modal",
-        "backbone-forms-list"  : "../../libs/backbone-forms/distribution.amd/editors/list.min",
-        bootstrapTemplate      : "../../libs/backbone-forms/distribution/templates/bootstrap3",
-        xmljs                  : "../../libs/xmljs/xmllint",
-        bootstrapAdapter       : "../../libs/bootstrapAdapter/index",
-        nanoscroller           : "../../libs/nanoscroller/dist/javascripts/jquery.nanoscroller",
-        typeahead              : "../../libs/typeahead/bootstrap3-typeahead",
-        "backbone.radio"       : "../../libs/backbone.radio/build/backbone.radio",
-        fuelux                 : "../../libs/fuelux/dist/js/fuelux",
-        'difflib'              : '../../libs/jsdifflib/difflib',
-        'diffview'             : '../../libs/jsdifflib/diffview',
-        'autocompleteTreeView' : '../../libs/autocompleteTreeview/Scripts/jquery.autocompTree'
+        backbone: "../../libs/backbone/backbone",
+        blobjs: "../../libs/blobjs/Blob",
+        bootstrap: "../../libs/bootstrap/dist/js/bootstrap",
+        fancytree: "../../libs/fancytree/dist/jquery.fancytree-all.min",
+        filesaver: "../../libs/filesaver/FileSaver",
+        i18n: "../../libs/i18n/i18next",
+        jquery: "../../libs/jquery/dist/jquery",
+        "jquery-ui": "../../libs/jquery-ui/jquery-ui",
+        underscore: "../../libs/underscore/underscore",
+        "NS.UI.Navbar": "../../libs/NS.UI.Navbar/navbar",
+        "NS.UI.NavbarTheme": "../../libs/NS.UI.Navbar/themes/navbar.bootstrap3",
+        "NS.UI.Notification": "../../libs/NS.UI.Notification/notification",
+        requirejs: "../../libs/requirejs/require",
+        "font-awesome": "../../libs/font-awesome/fonts/*",
+        "backbone-forms": "../../libs/backbone-forms/distribution.amd/backbone-forms",
+        modalAdapter: "../../libs/bootstrapAdapter/src/backbone.bootstrap-modal",
+        "backbone-forms-list": "../../libs/backbone-forms/distribution.amd/editors/list.min",
+        bootstrapTemplate: "../../libs/backbone-forms/distribution/templates/bootstrap3",
+        xmljs: "../../libs/xmljs/xmllint",
+        bootstrapAdapter: "../../libs/bootstrapAdapter/index",
+        nanoscroller: "../../libs/nanoscroller/dist/javascripts/jquery.nanoscroller",
+        typeahead: "../../libs/typeahead/bootstrap3-typeahead",
+        "backbone.radio": "../../libs/backbone.radio/build/backbone.radio",
+        fuelux: "../../libs/fuelux/dist/js/fuelux",
+        difflib: "../../libs/jsdifflib/difflib",
+        diffview: "../../libs/jsdifflib/diffview",
+        autocompleteTreeView: "../../libs/autocompleteTreeview/Scripts/jquery.autocompTree",
+        "jquery-simple-slider": "../../libs/jquery-simple-slider/js/simple-slider",
     },
+    shim: {
+        blobjs: {
+            exports: "blobjs"
+        },
+        filesaver: {
+            exports: "filesaver"
+        },
+        difflib: {
+            exports: "difflib"
+        },
+        diffview: {
+            exports: "diffview"
+        },
+        jquery: {
+            exports: "$"
+        },
+        underscore: {
+            exports: "_"
+        },
+        backbone: {
+            exports: "Backbone",
+            deps: [
+                "underscore",
+                "jquery"
+            ]
+        },
+        "backbone.radio": {
+            deps: [
+                "backbone"
+            ],
+            exports: "Backbone"
+        },
+        "jquery-ui": {
+            exports: "$",
+            deps: [
+                "jquery"
+            ]
+        },
+        fancytree: {
+            deps: [
+                "jquery-ui"
+            ],
+            exports: "$"
+        },
+        i18n: {
+            exports: "$",
+            deps: [
+                "jquery"
+            ]
+        },
+        "NS.UI.Navbar": {
+            exports: "$",
+            deps: [
+                "jquery",
+                "backbone",
+                "bootstrap"
+            ]
+        },
+        "NS.UI.NavbarTheme": {
+            deps: [
+                "NS.UI.Navbar"
+            ],
+            exports: "NS"
+        },
+        "NS.UI.Notification": {
+            exports: "$",
+            deps: [
+                "jquery",
+                "backbone",
+                "bootstrap"
+            ]
+        },
+        bootstrap: {
+            exports: "$",
+            deps: [
+                "jquery"
+            ]
+        },
+        typeahead: {
+            exports: "$",
+            deps: [
+                "jquery",
+                "bootstrap"
+            ]
+        },
+        nanoscroller: {
+            exports: "$",
+            deps: [
+                "jquery"
+            ]
+        },
+        autocompleteTreeView: {
+            deps: [
+                "jquery",
+                "jquery-ui",
+                "fancytree"
+            ],
+            exports: "$"
+        },
+        "jquery-simple-slider": {
+            exports: "$",
+            deps: [
+                "jquery"
+            ]
+        }
+    },
+    packages: [
 
-   shim: {
-        blobjs                : { exports : 'blobjs' },
-        filesaver             : { exports : 'filesaver' },
-        difflib               : { exports : 'difflib' },
-        diffview              : { exports : 'diffview'},
-        jquery                : { exports: "$"},
-        underscore            : { exports: "_"},
-        backbone              : { exports: "Backbone", deps: ["underscore", "jquery"] },
-        "backbone.radio"      : { deps: ["backbone"], exports: "Backbone"},
-        "jquery-ui"           : { exports: "$", deps: ["jquery"] },
-        fancytree             : { deps: ["jquery-ui"], exports: "$" },
-        i18n                  : { exports: "$", deps: ["jquery"] },
-        "NS.UI.Navbar"        : { exports: "$", deps: ["jquery", "backbone", "bootstrap"] },
-        "NS.UI.NavbarTheme"   : { deps: ["NS.UI.Navbar"], exports: "NS"},
-        "NS.UI.Notification"  : { exports: "$", deps: ["jquery", "backbone", "bootstrap"] },
-        bootstrap             : { exports: "$", deps: ["jquery"] },
-        typeahead             : { exports: "$", deps: ["jquery", "bootstrap"] },
-        nanoscroller          : { exports: "$", deps: ["jquery"] },
-        autocompleteTreeView  : { deps: ['jquery', 'jquery-ui', 'fancytree'], exports : "$"}
-    }
+    ]
 });
 
 require(['formbuilder'], function(formbuilder) {
