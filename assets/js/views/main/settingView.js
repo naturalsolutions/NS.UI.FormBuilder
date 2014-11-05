3a25229bff811abe84e19fac33eadc710314f2d0
amiceli <miceli.antoine@gmail.com>
2014-11-05 16:33:21 +0100

    Create a view for setting panel

---
+++ assets/js/views/main/settingView.js
@@ -0,0 +1,181 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'backbone.radio',
+    'text!../../../templates/main/settingView.html',
+    'jquery-ui',
+    'i18n',
+], function($, _, Backbone, Radio, settingTemplate) {
+
+    var SettingView = Backbone.View.extend({
+
+        events : {
+            'change select' : 'selectChanged'
+        },
+
+        initialize : function(options) {
+
+            this.el       = options.el;
+            this.URL      = options.URL;
+            this.template = _.template(settingTemplate);
+            this.form     = null;
+
+            //  Init radio channel
+            this.mainChannel = Backbone.Radio.channel('global');
+
+            this.mainChannel.on('saveConfiguration', _.bind(function() {
+                this.mainChannel.trigger('fieldConfiguration', this.form.model.toJSON());
+            }, this));
+
+            this.mainChannel.on('getModel:return', _.bind(function(field) {
+
+                //  Create form with model
+                this.initForm(field);
+
+            }, this));
+
+            this.mainChannel.on('saveForm', _.bind(function() {
+                //  Try to save change
+                //  Event send from router
+                var isValid = this.form.commit() === undefined;
+                if (isValid) {
+                    //field.set(this.form.getValue());
+                    //field = null;
+                    this.removeForm();
+                    this.mainChannel.trigger('formCommit', isValid)
+                }
+            }, this));
+
+            this.fieldWithSameType = null;
+
+            _.bindAll(this, 'render', 'createForm', 'initForm', 'selectChanged', 'removeForm', 'resetSelect');
+        },
+
+        render : function(options) {
+            var renderedContent = this.template();
+            $(this.el).html(renderedContent);
+            return this;
+        },
+
+        initForm : function(field) {
+            $.getJSON(this.URL.preConfiguredField, _.bind(function(fieldList) {
+
+                this.fieldWithSameType = fieldList[field.constructor.type];
+                this.resetSelect();
+
+                _.each(this.fieldWithSameType, _.bind(function(el, idx) {
+
+                    this.$el.find('select').append('<option value="' + idx + '">' + idx + '</option>');
+
+                }, this));
+
+            }, this)).fail( _.bind(function() {
+                this.$el.find('select').append('<option>No field found</option>');
+                this.$el.find('select').prop('disabled', true)
+            }, this)).always(_.bind(function() {
+
+                //  Always create form even if saved Field cannot be load
+                this.createForm(field);
+
+            }, this))
+        },
+
+        createForm : function(field) {
+            require(['backbone-forms', "backbone-forms-list", 'modalAdapter'], _.bind(function() {
+
+                if (this.form !== null) {
+                    //  Remove last form and create new with new model
+                    this.removeForm()
+                    this.form = new Backbone.Form({
+                        model: field,
+                    }).render();
+                } else {
+                    //  The form are not created yet
+                    this.form = new Backbone.Form({
+                        model: field,
+                    }).render();
+                }
+
+                $('.settings #form').append(this.form.el)
+                $('.settings #form').addClass('nano-content')
+
+                this.mainChannel.trigger('formCreated');
+
+                this.form.$el.on('change input[name="decimal"]', _.bind(function(e) {
+                    if ($(e.target).is(':checked')) {
+                        this.form.$el.find('.field-precision').addClass('advanced');
+                    } else {
+                        this.form.$el.find('.field-precision').removeClass('advanced');
+                    }
+                }, this))
+
+
+                if (_.contains(['Thesaurus', 'AutocompleteTreeView'], field.constructor.type)) {
+
+                    $.getJSON('ressources/thesaurus/thesaurus.json', _.bind(function(data) {
+
+                        $('.settings form input[name="defaultNode"]').replaceWith('<div id="defaultNode"></div>');
+                        $('.settings form #defaultNode').fancytree({
+                            source: data['d'],
+                            checkbox : false,
+                            selectMode : 1,
+                            activate : _.bind(function(event, data) {
+                                this.mainChannel.trigger('nodeSelected' + field.get('id'), data);
+                            }, this)
+                        });
+
+                    }, this)).error(function(a,b , c) {
+                        alert ("can't load ressources !");
+                    })
+
+                } else if (field.constructor.type === 'TreeView') {
+
+                    $('.settings form input[name="defaultNode"]').replaceWith('<div id="defaultNode"></div>');
+                    $('.settings form #defaultNode').fancytree({
+                        source: [
+                            {title: "Node 1", key: "1"},
+                            {title: "Folder 2", key: "2", folder: true, children: [
+                                {title: "Node 2.1", key: "3"},
+                                {title: "Node 2.2", key: "4"}
+                            ]}
+                        ],
+                        selectMode : 1,
+                        activate : _.bind(function(event, data) {
+                            field.set('defaultNode', data.node.key)
+                            this.mainChannel.trigger('nodeSelected' + field.get('id'), data);
+                        }, this)
+                    });
+
+                }
+
+            }, this));
+        },
+
+        removeForm : function() {
+            this.form.undelegateEvents();
+            this.form.$el.removeData().unbind();
+            this.form.remove();
+            Backbone.View.prototype.remove.call(this.form);
+            this.form = null;
+            this.$el.find('#form').html('');
+        },
+
+        resetSelect : function() {
+            this.$el.find('select').html('<option value="" disabled selected>Select an option</option>');
+        },
+
+        selectChanged : function(e) {
+            var choice = this.fieldWithSameType[ $(e.target).val() ];
+
+            this.form.setValue(choice)
+            if(_.contains(choice['validators'], "required")) {
+                this.form.setValue({'required': true});
+            }
+        }
+
+    });
+
+    return SettingView;
+
+});
