3951a5d68be2b4e5f4fac3fb0a635881babdbfff
amiceli <miceli.antoine@gmail.com>
2015-01-22 12:13:25 +0100

    remove jsdifflib

--- assets/js/editionPageModule/utilities/Utilities.js
+++ assets/js/editionPageModule/utilities/Utilities.js
@@ -7,39 +7,9 @@
 * @version         1.0
 */
 
-define(['jquery', 'underscore', 'difflib', 'diffview'], function($, _, difflib, diffview) {
+define(['jquery', 'underscore'], function($, _) {
 
     var utilities = {};
-
-    /**
-    *
-    * @param {type} baseTText
-    * @param {type} newText
-    * @param {type} selector
-    * @param {type} contextSize
-    * @param {type} baseTextName
-    * @param {type} newTextName
-    * @param {type} inline
-    * @returns {diffview.buildView.ctelt.e|Element|diffview.buildView.celt.e}
-    */
-    utilities.GetXMLDiff = function (baseText, newText, baseTextName, newTextName, inline, contextSize) {
-        var base    = difflib.stringAsLines(baseText),
-        newtxt  = difflib.stringAsLines(newText),
-        sm      = new difflib.SequenceMatcher(base, newtxt),
-        opcodes = sm.get_opcodes();
-
-        // build the diff view and add it to the current DOM
-        return diffview.buildView({
-            baseTextLines   : base,
-            newTextLines    : newtxt,
-            opcodes         : opcodes,
-            // set the display titles for each resource
-            baseTextName    : baseTextName  || 'Source file',
-            newTextName     : newTextName   || 'Updated file',
-            contextSize     : contextSize   || null,
-            viewType        : inline        ||  0
-        });
-    };
 
     utilities.ReadFile = function(input, callback) {
         if (input.files && input.files[0]) {
