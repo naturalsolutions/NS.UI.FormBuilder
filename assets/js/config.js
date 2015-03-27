f2674f336023dcfd3be170eb99806d26ae131525
amiceli <miceli.antoine@gmail.com>
2015-03-27 14:16:01 +0100

    add datetimepicker reference

--- assets/js/config.js
+++ assets/js/config.js
@@ -10,33 +10,34 @@
 require.config({
 
     paths: {
-        backbone               : "../../libs/backbone/backbone",
-        blobjs                 : "../../libs/blobjs/Blob",
-        bootstrap              : "../../libs/bootstrap/dist/js/bootstrap.min",
-        fancytree              : "../../libs/fancytree/dist/jquery.fancytree-all.min",
-        filesaver              : "../../libs/filesaver/FileSaver.min",
-        i18n                   : "../../libs/i18n/i18next.min",
-        jquery                 : "../../libs/jquery/jquery.min",
-        "jquery-ui"            : "../../libs/jquery-ui/jquery-ui",
-        underscore             : "../../libs/underscore/underscore-min",
-        requirejs              : "../../libs/requirejs/require",
-        "backbone-forms"       : "../../libs/backbone-forms/distribution.amd/backbone-forms",
-        modalAdapter           : "../../libs/bootstrapAdapter/src/backbone.bootstrap-modal",
-        "backbone-forms-list"  : "../../libs/backbone-forms/distribution.amd/editors/list.min",
-        bootstrapTemplate      : "../../libs/backbone-forms/distribution/templates/bootstrap3",
-        bootstrapAdapter       : "../../libs/bootstrapAdapter/index",
-        "backbone.radio"       : "../../libs/backbone.radio/build/backbone.radio",
-        fuelux                 : "../../libs/fuelux/dist/js/fuelux",
-        difflib                : "../../libs/jsdifflib/difflib",
-        diffview               : "../../libs/jsdifflib/diffview",
-        autocompleteTreeView   : "../../libs/autocompleteTreeview/Scripts/jquery.autocompTree",
-        "jquery-simple-slider" : "../../libs/jquery-simple-slider/js/simple-slider",
-        "bootstrap-select"     : "../../libs/bootstrap-select/dist/js/bootstrap-select",
-        sweetalert             : "../../libs/sweetalert/lib/sweet-alert",
-        marionette             : '../../libs/marionette/lib/backbone.marionette.min',
-        backgrid               : '../../libs/backgrid/lib/backgrid',
-        moment                 : '../../libs/moment/moment',
-        slimScroll             : '../../libs/slimScroll/jquery.slimscroll.min'
+        backbone                            : "../../libs/backbone/backbone",
+        blobjs                              : "../../libs/blobjs/Blob",
+        bootstrap                           : "../../libs/bootstrap/dist/js/bootstrap.min",
+        fancytree                           : "../../libs/fancytree/dist/jquery.fancytree-all.min",
+        filesaver                           : "../../libs/filesaver/FileSaver.min",
+        i18n                                : "../../libs/i18n/i18next.min",
+        jquery                              : "../../libs/jquery/jquery.min",
+        "jquery-ui"                         : "../../libs/jquery-ui/jquery-ui",
+        underscore                          : "../../libs/underscore/underscore-min",
+        requirejs                           : "../../libs/requirejs/require",
+        "backbone-forms"                    : "../../libs/backbone-forms/distribution.amd/backbone-forms",
+        modalAdapter                        : "../../libs/bootstrapAdapter/src/backbone.bootstrap-modal",
+        "backbone-forms-list"               : "../../libs/backbone-forms/distribution.amd/editors/list.min",
+        bootstrapTemplate                   : "../../libs/backbone-forms/distribution/templates/bootstrap3",
+        bootstrapAdapter                    : "../../libs/bootstrapAdapter/index",
+        "backbone.radio"                    : "../../libs/backbone.radio/build/backbone.radio",
+        fuelux                              : "../../libs/fuelux/dist/js/fuelux",
+        difflib                             : "../../libs/jsdifflib/difflib",
+        diffview                            : "../../libs/jsdifflib/diffview",
+        autocompleteTreeView                : "../../libs/autocompleteTreeview/Scripts/jquery.autocompTree",
+        "jquery-simple-slider"              : "../../libs/jquery-simple-slider/js/simple-slider",
+        "bootstrap-select"                  : "../../libs/bootstrap-select/dist/js/bootstrap-select",
+        sweetalert                          : "../../libs/sweetalert/lib/sweet-alert",
+        marionette                          : '../../libs/marionette/lib/backbone.marionette.min',
+        backgrid                            : '../../libs/backgrid/lib/backgrid',
+        moment                              : '../../libs/moment/moment',
+        slimScroll                          : '../../libs/slimScroll/jquery.slimscroll.min',
+        "eonasdan-bootstrap-datetimepicker" : "../../libs/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min"
     },
 
     shim: {
@@ -121,6 +122,12 @@ require.config({
                 "jquery"
             ]
         },
+        "eonasdan-bootstrap-datetimepicker": {
+            exports: "$",
+            deps: [
+                "jquery"
+            ]
+        },
         sweetalert: {
             exports: "$",
             deps: [
@@ -138,7 +145,7 @@ require.config({
     }
 });
 
-require(['formbuilder'], function(formbuilder) {
+require(['jquery', 'Translater', 'formbuilder'], function($, Translater, formbuilder) {
 
     var options = {
 
@@ -150,13 +157,17 @@ require(['formbuilder'], function(formbuilder) {
             autocompleteURL       : 'ressources/autocomplete/',
 
             //  Allows to get translation ressources (use i18nnext : http://i18next.com/ )
-            translationURL        : 'ressources/locales/',
+            translationURL        : 'ressources/locales/__lng__/__ns__.json',
 
             //  Get form keywords autocomplete values
             keywordAutocomplete   : 'ressources/autocomplete/keywords.json',
 
             //  Get all form name for autocomplete
             protocolAutocomplete  : 'ressources/autocomplete/protocols.json',
+
+            //  Get all form name for autocomplete
+            //  Not works if you are in client side mode only
+            formSaveURL  : '/formbuilderWS/form',
 
             //  Get all unities for autocomplete
             unitURL               : 'ressources/autocomplete/units.json',
@@ -176,7 +187,7 @@ require(['formbuilder'], function(formbuilder) {
 
         //  Wich parent HTML element for the application
         el : '#formBuilder'
-    }
+    };
 
     formbuilder.start(options);
 });
