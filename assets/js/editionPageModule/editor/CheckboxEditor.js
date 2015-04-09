bba8ae55f1f74b4bc29d0e39ec44a5f24b959fc0
amiceli <miceli.antoine@gmail.com>
2015-04-09 17:17:02 +0200

    add editor

---
+++ assets/js/editionPageModule/editor/CheckboxEditor.js
@@ -0,0 +1,49 @@
+define(['jquery', 'underscore', 'backbone', 'backbone-forms'], function($, _, Backbone, Form) {
+
+    var CheckboxEditor = Backbone.Form.editors.Base.extend({
+        tagName: 'input',
+
+        initialize: function(options) {
+            this.options = options;
+
+            this.options.schema = this.options.schema === undefined ? _.pick(options, 'editorClass', 'fieldClass', 'iconClass', 'title') : this.options.schema;
+
+            Backbone.Form.editors.Base.prototype.initialize.call(this, options);
+            this.template = this.constructor.template;
+        },
+
+        render: function() {
+            //  Set default value and verifying type
+            this.value = (typeof this.value == 'boolean') ? this.value : false;
+            this.setElement(
+                this.template({
+                    id          : this.options.id,
+                    editorClass : this.options.schema.editorClass || '',
+                    fieldClass  : this.options.schema.fieldClass || 'form-group',
+                    iconClass   : this.options.iconClass || '',
+                    value       : this.value,
+                    title       : this.schema.title == undefined ? this.key : this.schema.title
+                })
+            );
+            return this;
+        },
+
+        getValue: function() {
+            return this.$el.find('input').is(':checked');
+        },
+
+        setValue: function(value) {
+            this.$el.find('input').prop('checked', value);
+        }
+
+    }, {
+        template: _.template(
+            '<div class="checkboxField">' +
+                '<input type="checkbox" id="<%= id %>" name="<%= id %>" class="<%= editorClass %>" <% if(value){%>checked<%}%> />' +
+                '<label for="<%= id %>" class="<%= iconClass %>">' +
+                    '&nbsp;<%= title %></label>' +
+            '</div>')
+    });
+
+    return CheckboxEditor;
+});
