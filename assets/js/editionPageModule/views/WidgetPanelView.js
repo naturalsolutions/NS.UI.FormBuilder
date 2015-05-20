1efd726b4e8ab84f2f9ba36b0d4f949e864228a3
amiceli <miceli.antoine@gmail.com>
2015-05-20 11:20:57 +0200

    [#94488312] Fix duplicated field bug when user clicks on widget panel pictos

--- assets/js/editionPageModule/views/WidgetPanelView.js
+++ assets/js/editionPageModule/views/WidgetPanelView.js
@@ -3,51 +3,87 @@ define([
     'marionette',
     'text!../templates/WidgetPanelView.html',
     '../models/Fields',
+    '../../Translater',
     'backbone.radio',
+    'sweetalert',
     'i18n',
     'jquery-ui',
-    'sweetalert'
-], function($, Marionette, WidgetPanelViewTemplate, Fields, Radio) {
 
+], function($, Marionette, WidgetPanelViewTemplate, Fields,Translater, Radio, swal) {
 
+    //  Get singleton Translater object
+    var translater = Translater.getTranslater();
+
+    /**
+     * Widget Panel contains contains avlaible field of the application
+     */
     var WidgetPanelView = Backbone.Marionette.ItemView.extend({
 
+        /**
+         * View events
+         */
         events: {
-            'click div.col-md-5'       : 'appendToDrop',
-            'click div.col-md-10'      : 'appendToDrop',
-            'click #smallFeatures div' : 'appendToDrop',
-            'click h3'                 : 'displayContent'
+            'click div.col-md-5'                 : 'appendToDrop',
+            'click div.col-md-10'                : 'appendToDrop',
+            'click #smallFeatures .scroll > div' : 'appendToDrop',
+            'click h3'                           : 'displayContent'
         },
 
+        /**
+         * Override template function with html template
+         *
+         * @returns {function}
+         */
         template : function() {
             return _.template(WidgetPanelViewTemplate)({
                 section : this.section
             });
         },
 
-
+        /**
+         * Initialize view
+         *
+         * @param options some options like template
+         */
         initialize : function(options) {
-            this.collection = options.fieldCollection;
-            _.bindAll(this, 'template')
+            _.bindAll(this, 'template');
 
             this.initSection();
             this.initFormChannel();
         },
 
+        /**
+         * Init form channel
+         */
         initFormChannel : function() {
+            //  The widget panel view used this channel to send event when an user wants to add a field
             this.formChannel = Backbone.Radio.channel('form');
         },
 
+        /**
+         * Click event callback
+         * Launch when user wants to add a field in the form
+         *
+         * @param e jQuery event
+         */
         appendToDrop : function(e) {
             var elementClassName = $(e.target).data("type");
 
+            //  Check if the field type is a valid type
             if (Fields[elementClassName] !== undefined) {
                 this.formChannel.trigger('addElement', elementClassName);
             } else {
-                swal("Une erreur est survenue !", "Le champ n'a pas pu être généré !", "error");
+                swal(
+                    translater.getValueFromKey('modal.field.error') || "Echec de l'ajout!",
+                    translater.getValueFromKey('modal.field.errorMsg') || "Une erreur est survenue lors de l'ajout du champ !",
+                    "error"
+                );
             }
         },
 
+        /**
+         * Get Fields information, like section, i18n translation ...
+         */
         initSection : function() {
             var section = { standard : {}, other : {} };
 
@@ -76,6 +112,13 @@ define([
             this.section = section;
         },
 
+        /**
+         * Accordion callback
+         *
+         * I create a small accordion because the jquery accordion CSS breaks my application style
+         *
+         * @param e jQuery event
+         */
         displayContent : function(e) {
             var accordion = $(e.currentTarget).data('accordion')
             $('.section[data-accordion!="content-' + accordion + '"]').slideUp(500, function() {
@@ -83,11 +126,18 @@ define([
             });
         },
 
+        /**
+         * Enable accordion
+         */
         initAccordion : function() {
             this.$el.find('.section').not(':first').hide();
         },
 
-
+        /**
+         * Render view
+         *
+         * @param options options object
+         */
         onRender : function(options) {
             // run i18nnext translation in the view context
             this.$el.i18n();
