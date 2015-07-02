d1d5ad015b22384372b4aea754ec4f91f8ee6c85
amiceli <miceli.antoine@gmail.com>
2015-07-02 12:38:03 +0200

    [#97615456] Update toggle style and animation

--- assets/js/editionPageModule/layout/EditionPageLayout.js
+++ assets/js/editionPageModule/layout/EditionPageLayout.js
@@ -27,8 +27,8 @@ define([
          * @type {Object}
          */
         events : {
-            'click #toggle span.open'   : 'minimizeWidgetPanel',
-            'click #toggle span.closed' : 'maximizeWidgetPanel'
+            'click #toggle.open'   : 'minimizeWidgetPanel',
+            'click #toggle.closed' : 'maximizeWidgetPanel'
         },
 
 
@@ -172,7 +172,7 @@ define([
             $('#formPanel').switchClass('col-md-8', 'col-md-11', 300);
             $('#widgetPanel').switchClass('col-md-4', 'col-md-1', 300);
             $('#widgetPanel #features').fadeOut(200);
-            $('#toggle span').switchClass('open', 'closed');
+            $('#toggle').switchClass('open', 'closed');
         },
 
 
@@ -183,7 +183,7 @@ define([
             $('#formPanel').switchClass('col-md-11', 'col-md-8', 300);
             $('#widgetPanel').switchClass('col-md-1', 'col-md-4', 300, function() {
                 $('#widgetPanel #features').fadeIn(200);
-                $('#toggle span').switchClass('closed', 'open');
+                $('#toggle').switchClass('closed', 'open');
             });
 
         },
