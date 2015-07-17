4b2a65da7be77663bef83e5177e2683a549f4603
amiceli <miceli.antoine@gmail.com>
2015-07-17 11:17:51 +0200

    [#98654192] Harmonize scroll bar position on edition page.

--- assets/js/editionPageModule/views/WidgetPanelView.js
+++ assets/js/editionPageModule/views/WidgetPanelView.js
@@ -142,9 +142,11 @@ define([
             // run i18nnext translation in the view context
             this.$el.i18n();
 
-            //  Add scrollbar
+            //  Add scroll bar
             this.$el.find('.scroll').slimScroll({
-                height : '90%'
+                height       : '88%',
+                railVisible  : true,
+                alwaysVisible: true
             });
 
             //  Disable selection on field element
