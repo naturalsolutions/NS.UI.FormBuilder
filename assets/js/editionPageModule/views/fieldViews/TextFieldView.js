4c5bc3f18c222769274e830846ec01db30f63ed5
rkjdid <romainknezevic@gmail.com>
2017-11-14 14:59:16 +0100

    TextFieldView: fix the way "isDefaultSQL" is updated
      + misc simplifications

--- assets/js/editionPageModule/views/fieldViews/TextFieldView.js
+++ assets/js/editionPageModule/views/fieldViews/TextFieldView.js
@@ -5,66 +5,33 @@ define([
     'editionPageModule/views/fieldViews/BaseView',
     'text!editionPageModule/templates/fields/TextFieldView.html',
     'text!editionPageModule/templates/fields/readonly/TextFieldView.html',
-    '../../../../../node_modules/sqlite-parser/dist/sqlite-parser'
+    '../../../../../node_modules/sqlite-parser/dist/sqlite-parser' /* <-- TODO */
 ], function($, _, Backbone, BaseView, viewTemplate, viewTemplateRO, sqliteParser) {
 
     var TextFieldView = BaseView.extend({
-
-        /**
-         * Get BaseView events and add sepecific TextFieldView event
-         */
-        events: function() {
-            return _.extend({}, BaseView.prototype.events, {
-                'change input[type="text"]': 'updateModel'
-            });
-        },
-
         initialize : function(options, readonly) {
-            var opt = options;
-            opt.template = viewTemplate;
-            if (readonly)
-                opt.template = viewTemplateRO
-            BaseView.prototype.initialize.apply(this, [opt]);
+            BaseView.prototype.initialize.apply(this, [options, readonly]);
+            this.model.on('change:defaultValue', _.bind(this.updateIsDefaultSql, this));
         },
 
-        /**
-         * Render view
-         */
-        render : function() {
-            BaseView.prototype.render.apply(this, arguments);
-
-            var sqlParsed = false;
-
-            try
-            {
-                sqlParsed = sqliteParser(this.model.get('defaultValue'));
-            }
-            catch (err)
-            {
-                console.log(err);
+        updateIsDefaultSql: function(model, val) {
+            if (!val) {
+                model.set('isDefaultSQL', false);
+                return;
             }
 
-            if (sqlParsed)
-            {
-                this.model.set('isDefaultSQL', true);
+            var parsed = false;
+            try {
+                sqliteParser(val);
+                parsed = true;
+            } catch (err) {
+                // todo really log an error ? we're not sure user wants to SQL
+                //      and if he does, maybe he wants a swal alert
+                console.warn("sql error for field '" + model.get('name') + "'", err);
             }
-            else
-            {
-                this.model.set('isDefaultSQL', false);
-            }
-        },
-
-       /**
-        * Change model value when text input value changed
-        *
-        * @param {object} jQuery event
-        */
-        updateModel: function(e) {
-            this.model.set('value', $(e.target).val());
+            this.model.set('isDefaultSQL', parsed);
         }
-
     });
 
     return TextFieldView;
-
 });
