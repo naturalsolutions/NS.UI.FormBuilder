408634e7e1f6de77b1a062feff0f6790fcd774e5
rkjdid <romainknezevic@gmail.com>
2017-11-20 16:34:10 +0100

    disregard language for units loader
    since localized unit value is stored raw in database's FieldProperty value,
    we have no way of binding a translation to another.

--- assets/js/editionPageModule/views/loaders/TrackLoader.js
+++ assets/js/editionPageModule/views/loaders/TrackLoader.js
@@ -2,6 +2,12 @@ define(['jquery'], function ($) {
     return {
         loadFormData: function(form, urls, lang) {
             if (form.fields.unity) {
+                if (lang !== 'fr') {
+                    // todo the way units are handled, language specific units cannot work:
+                    // localized value is stored in database, we cannot match translations
+                    console.warn("disregarding language for fetching units, not implemented (forced 'fr')");
+                    lang = 'fr';
+                }
                 this.loadUnits(form.fields.unity, urls.unities + "/track/" + lang);
             }
         },
