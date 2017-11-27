1f93a8033809766fae1a3a09bdd6a928d2124ca8
rkjdid <romainknezevic@gmail.com>
2017-11-27 12:17:41 +0100

    CheckboxEditor: add handlers mechanism on input change

--- assets/js/editionPageModule/editor/CheckboxEditor.js
+++ assets/js/editionPageModule/editor/CheckboxEditor.js
@@ -12,16 +12,25 @@ define([
 
         render: function() {
             this.value = (typeof this.value == 'boolean') ? this.value : false;
-            this.setElement(
-                _.template(CheckboxTemplate)({
-                    id          : this.options.id,
-                    editorClass : this.options.schema.editorClass || '',
-                    fieldClass  : this.options.schema.fieldClass || 'form-group',
-                    iconClass   : this.options.iconClass || '',
-                    value       : this.value,
-                    title       : this.schema.title == undefined ? this.key : this.schema.title
-                })
-            );
+            this.$el = $(_.template(CheckboxTemplate)({
+                id          : this.options.id,
+                editorClass : this.options.schema.editorClass || '',
+                fieldClass  : this.options.schema.fieldClass || 'form-group',
+                iconClass   : this.options.iconClass || '',
+                value       : this.value,
+                title       : this.schema.title == undefined ? this.key : this.schema.title
+            }));
+
+            // subscribe handlers from schema on input change
+            if (this.options.schema.handlers) {
+                var handlers = this.options.schema.handlers;
+                for (var i in handlers) {
+                    this.$el.find("input").on("change", _.bind(function(e) {
+                        handlers[i](e.target.checked);
+                    }, this));
+                }
+            }
+            this.setElement(this.$el);
             return this;
         },
 
