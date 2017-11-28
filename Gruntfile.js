f74a04604e65402f172f8b09eefef81996316897
rkjdid <romainknezevic@gmail.com>
2017-11-28 11:16:47 +0100

    start dependencies cleaning:
      - clean gruntfile & package.json
      - add webpack dep
      - remove bowerrc, bower.json (we'll use yarn)

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,24 +39,14 @@ module.exports = function(grunt) {
                 files: ['assets/stylesheet/**/*.less'],
                 tasks: ['less:dev', 'autoprefixer:single_file']
             }
-        },
-
-        autoprefixer: {
-            single_file: {
-                src: "compressed/formbuilder.min.css",
-                dest: "compressed/prefixedformbuilder.min.css"
-            }
         }
     });
 
-    grunt.registerTask('default', ['less:dev', 'autoprefixer']);
-    grunt.registerTask('dev', ['less:dev', 'autoprefixer']);
-    grunt.registerTask('prod', ['less:dist', 'autoprefixer']);
+    grunt.registerTask('default', 'dev');
+    grunt.registerTask('dev', ['less:dev']);
+    grunt.registerTask('prod', ['less:dist']);
 	grunt.registerTask('build', 'prod');
 
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-bower-clean');
-
 }
