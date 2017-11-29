481ea4ab1bd681dcd837aaba360b09ade343a526
rkjdid <romainknezevic@gmail.com>
2017-11-29 18:32:09 +0100

    killall sqliteparser & weird features

--- assets/js/editionPageModule/views/fieldViews/SelectFieldView.js
+++ assets/js/editionPageModule/views/fieldViews/SelectFieldView.js
@@ -1,40 +1,5 @@
 define([
-    'jquery',
-    'lodash',
-    'backbone',
-    'editionPageModule/views/fieldViews/BaseView',
-    '../../../../../node_modules/sqlite-parser/dist/sqlite-parser',
-], function($, _, Backbone, BaseView, sqliteParser) {
-
-    var SelectFieldView = BaseView.extend({
-
-        render : function() {
-            BaseView.prototype.render.apply(this, arguments);
-            if (this.model.get('defaultValue'))
-            {
-                var sqlParsed = false;
-
-                try
-                {
-                    if (this.model.get('defaultValue').constructor != Array)
-                        sqlParsed = sqliteParser(this.model.get('defaultValue'));
-                }
-                catch (err)
-                {
-                    console.log(err);
-                }
-
-                if (sqlParsed)
-                {
-                    this.model.set('isDefaultSQL', true);
-                }
-                else
-                {
-                    this.model.set('isDefaultSQL', false);
-                }
-            }
-        }
-    });
-
-	return SelectFieldView;
+    'editionPageModule/views/fieldViews/BaseView'
+], function(BaseView) {
+    return BaseView;
 });
--- assets/js/editionPageModule/views/fieldViews/TextFieldView.js
+++ assets/js/editionPageModule/views/fieldViews/TextFieldView.js
@@ -1,35 +1,5 @@
 define([
-    'jquery',
-    'lodash',
-    'backbone',
-    'editionPageModule/views/fieldViews/BaseView',
-    '../../../../../node_modules/sqlite-parser/dist/sqlite-parser' /* <-- TODO */
-], function($, _, Backbone, BaseView, sqliteParser) {
-
-    var TextFieldView = BaseView.extend({
-        initialize : function(options, readonly) {
-            BaseView.prototype.initialize.apply(this, [options, readonly]);
-            this.model.on('change:defaultValue', _.bind(this.updateIsDefaultSql, this));
-        },
-
-        updateIsDefaultSql: function(model, val) {
-            if (!val) {
-                model.set('isDefaultSQL', false);
-                return;
-            }
-
-            var parsed = false;
-            try {
-                sqliteParser(val);
-                parsed = true;
-            } catch (err) {
-                // todo really log an error ? we're not sure user wants to SQL
-                //      and if he does, maybe he wants a swal alert
-                console.warn("sql error for field '" + model.get('name') + "'", err);
-            }
-            this.model.set('isDefaultSQL', parsed);
-        }
-    });
-
-    return TextFieldView;
+    'editionPageModule/views/fieldViews/BaseView'
+], function(BaseView) {
+    return BaseView;
 });
