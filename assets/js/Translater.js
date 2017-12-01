3309a461e53dedcd26a3d8c28819ed7ecb526ab1
rkjdid <romainknezevic@gmail.com>
2017-12-01 13:56:48 +0100

    update i18n usage

--- assets/js/Translater.js
+++ assets/js/Translater.js
@@ -1,15 +1,31 @@
 define([
-    'jquery', 'lodash', 'marionette', 'auth', 'i18n'
-], function($, _, Marionette, auth) {
+    'jquery', 'lodash', 'marionette', 'auth', 'i18n', 'i18n-jquery', 'i18n-xhr'
+], function($, _, Marionette, auth, i18n, i18njQuery, i18nXhr) {
 
     var Translater = Marionette.Object.extend({
-
+        loaded: false,
         initialize: function() {
-            this.url = 'ressources/locales/__lng__/__ns__.json';
             var lang = auth.userlanguage ||navigator.language ||
                 navigator.userLanguagenavigator.language ||
                 navigator.userLanguage || 'en';
-            i18n.init({ resGetPath: this.url, getAsync : false, lng : lang});
+
+            i18n.use(i18nXhr);
+            i18n.init({
+                lng : lang,
+                fallbackLng: 'en',
+                async: false,
+                getAsync: false,
+                backend: {
+                    loadPath: 'ressources/locales/{{lng}}/{{ns}}.json'
+                }
+            }, _.bind(function() {
+                i18njQuery.init(i18n, $, {
+                    tName: 't',
+                    handleName: 'i18n',
+                    selector: 'data-i18n'
+                });
+                this.loaded = true;
+            }, this));
         },
 
         getValueFromKey : function(key) {
@@ -20,7 +36,7 @@ define([
     var translater = new Translater();
 
     return {
-        getTranslater: function (options) { return translater; }
+        getTranslater: function () { return translater; }
     };
 
 });
