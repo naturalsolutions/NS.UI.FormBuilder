c58e1cfa37866869bd2058ff282f958b4ec2c835
rkjdid <romainknezevic@gmail.com>
2017-12-04 17:47:03 +0100

    reformat app-config.example.js (no change)

--- assets/js/app-config.example.js
+++ assets/js/app-config.example.js
@@ -1,4 +1,3 @@
-
 //
 //  Configuration file
 //
@@ -11,86 +10,86 @@
 define([], function() {
 
     var AppConfiguration = {
-        readonlyMode : false,								// Defines whether the app has to be displayed in Read Only mode or not
-        displayUserFilter : false,        					// (not implemented) Defines whether you display the User filter on the main page or not
-		authmode : 'classic',								// Authentication mode (portal or anything)
-		portalURL : 'http://localhost/nsportal/front/',		// Portal local URL
-		securityKey : '',									// JWT security secret word key
+        readonlyMode: false,                           // Defines whether the app has to be displayed in Read Only mode or not
+        displayUserFilter: false,                      // (not implemented) Defines whether you display the User filter on the main page or not
+        authmode: 'classic',                           // Authentication mode (portal or anything)
+        portalURL: 'http://localhost/nsportal/front/', // Portal local URL
+        securityKey: '',                               // JWT security secret word key
 
-        paths : {
+        paths: {
             // Path to the thesaurus Web Services
-            thesaurusWSPath : 'http://localhost/ThesaurusCore/api/thesaurus/fastInitForCompleteTree',
-			// POST(@lng, @StartNodeId)
-            positionWSPath : 'http://localhost/positionCore/api/PositionAction/GetTree'
+            thesaurusWSPath: 'http://localhost/ThesaurusCore/api/thesaurus/fastInitForCompleteTree',
+            // POST(@lng, @StartNodeId)
+            positionWSPath: 'http://localhost/positionCore/api/PositionAction/GetTree'
         },
 
         // Defines the list of rules that will apply to the forms
-        rules : [
+        rules: [
             {
-                error : {
-                    'title' : 'Form size exceeded',
-                    'content' : 'Form can\'t have more than 666 fields'
+                error: {
+                    'title': 'Form size exceeded',
+                    'content': 'Form can\'t have more than 666 fields'
                 },
 
                 /**
                  * @param form
                  **/
-                execute : function(form) {
+                execute: function(form) {
                     return form.length < 667;
                 }
             }
         ],
 
-        config : {
-            options : {
+        config: {
+            options: {
 
                 // Specify URL for formBuilder configuration
                 // Replace this URL with your own
 
-                URLOptions : {
+                URLOptions: {
                     // ***********************************************************************
                     // The app needs the following nodes to have a proper path to the server :
                     // ***********************************************************************
 
-                    formAutocomplete 		: '/FormbuilderWS/autocomplete/forms',
-                    forms 					: '/FormbuilderWS/forms',
-                    makeObsolete 			: '/FormbuilderWS/makeObsolete',
-                    allforms 				: '/FormbuilderWS/forms/allforms',
-                    formSaveURL  			: '/FormbuilderWS/forms',							//  Get all form name for autocomplete, does not work if you are in client side mode only
-                    preConfiguredField    	: '/FormbuilderWS/configurations', 					//  Returns all pre-configurated fields, they are fields saved by use for a future use, ex: an user create a firstName field because it will be present in many forms
-                    fieldConfigurationURL 	: '/FormbuilderWS/configurations',					//  Allow to send a pre-configurated field to the server. Send a POST request, won't work on client side, you need the back-end
-                    childforms              : '/FormbuilderWS/childforms',						// Child forms can get selected passing a parent form ID
-                    sqlAutocomplete			: '/FormbuilderWS/sqlAutocomplete', 				// Used to get values for autocomplete field
-                    unities            		: '/FormBuilderWS/unities',							// Get all unities for autocomplete
+                    formAutocomplete: '/FormbuilderWS/autocomplete/forms',
+                    forms: '/FormbuilderWS/forms',
+                    makeObsolete: '/FormbuilderWS/makeObsolete',
+                    allforms: '/FormbuilderWS/forms/allforms',
+                    formSaveURL: '/FormbuilderWS/forms',                    //  Get all form name for autocomplete, does not work if you are in client side mode only
+                    preConfiguredField: '/FormbuilderWS/configurations',    //  Returns all pre-configurated fields, they are fields saved by use for a future use ex: an user create a firstName field because it will be present in many forms
+                    fieldConfigurationURL: '/FormbuilderWS/configurations', //  Allow to send a pre-configurated field to the server. Send a POST request, won't work on client side, you need the back-end
+                    childforms: '/FormbuilderWS/childforms',                // Child forms can get selected passing a parent form ID
+                    sqlAutocomplete: '/FormbuilderWS/sqlAutocomplete',      // Used to get values for autocomplete field
+                    unities: '/FormBuilderWS/unities',                      // Get all unities for autocomplete
 
                     // ***********************************************************************
                     // RENECO Specific Paths :
                     // ***********************************************************************
 
-                    security				: '/FormbuilderWS/Security',						// Used for security chekings purpose (jwt among others ?)
-                    track					: '/FormbuilderWS/Track',							// Used for track data linking
-                    trackTypes				: '/FormbuilderWS/TrackTypes',						// Used to get track types (THIS PATH SHOULD BE TEMPORARY)
-                    trackFormWeight			: '/FormbuilderWS/Track/FormWeight',				// Used to get forms weight from track records
-                    trackInputWeight		: '/FormbuilderWS/Track/InputWeight',				// Used to get input weight from track records
+                    security: '/FormbuilderWS/Security',                    // Used for security chekings purpose (jwt among others ?)
+                    track: '/FormbuilderWS/Track',                          // Used for track data linking
+                    trackTypes: '/FormbuilderWS/TrackTypes',                // Used to get track types (THIS PATH SHOULD BE TEMPORARY)
+                    trackFormWeight: '/FormbuilderWS/Track/FormWeight',     // Used to get forms weight from track records
+                    trackInputWeight: '/FormbuilderWS/Track/InputWeight',   // Used to get input weight from track records
 
                     // ************************************************
                     // Still don't know what those nodes are used for :
                     // ************************************************
 
-                    autocompleteURL       	: 'ressources/autocomplete/', 						//  Allow to get some topic for autocomplete functionnalities
-                    translationURL        	: 'ressources/locales/__lng__/__ns__.json', 		//  Allows to get translation ressources (use i18nnext : http://i18next.com/ )
-                    keywordAutocomplete   	: 'ressources/autocomplete/keywords.json', 			//  Get form keywords autocomplete values
-                    usersAutocomplete 		: 'ressources/autocomplete/users.json',
-                    linkedFields 			: 'ressources/linkedFields/linkedFields.json',		//  Allow to get linked fields list
-                    templateUrl 			: 'ressources/templates/templates.json'				//  URL for form templates
+                    autocompleteURL: 'ressources/autocomplete/',                  //  Allow to get some topic for autocomplete functionnalities
+                    translationURL: 'ressources/locales/__lng__/__ns__.json',     //  Allows to get translation ressources (use i18nnext : http://i18next.com/ )
+                    keywordAutocomplete: 'ressources/autocomplete/keywords.json', //  Get form keywords autocomplete values
+                    usersAutocomplete: 'ressources/autocomplete/users.json',
+                    linkedFields: 'ressources/linkedFields/linkedFields.json',    //  Allow to get linked fields list
+                    templateUrl: 'ressources/templates/templates.json'            //  URL for form templates
                 },
 
                 //  Wich parent HTML element for the application
-                el 						: '#formBuilder'
+                el: '#formBuilder'
             }
         },
 
-        topcontext : 'reneco',
+        topcontext: 'reneco',
 
         /* list of all available input types
             inputTypes: [
@@ -188,7 +187,7 @@ define([], function() {
                     'Position'
                 ]
             },
-            ecollection : {
+            ecollection: {
                 inputTypes: [
                     'Autocomplete',
                     'Text',
@@ -220,5 +219,4 @@ define([], function() {
     };
 
     return AppConfiguration;
-
 });
