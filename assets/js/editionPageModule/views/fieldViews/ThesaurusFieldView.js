d8c2ea0f0d04eae0eeedd76cec19bbd13e372b95
rkjdid <romainknezevic@gmail.com>
2017-12-08 15:25:53 +0100

    ThesaurusField: display info popup on webservice url change

--- assets/js/editionPageModule/views/fieldViews/ThesaurusFieldView.js
+++ assets/js/editionPageModule/views/fieldViews/ThesaurusFieldView.js
@@ -2,9 +2,22 @@ define([
     'jquery',
     'lodash',
     'backbone',
+    'tools',
     'editionPageModule/views/fieldViews/BaseView'
-], function($, _, Backbone, BaseView) {
+], function($, _, Backbone, tools, BaseView) {
     return BaseView.extend({
+        events: _.extend(BaseView.prototype.events, {
+            "change input[name='webServiceURL']": "urlChanged"
+        }),
+
+        urlChanged: function(e) {
+            // preload new(?) url
+            tools.loadTree(e.target.value);
+
+            // tell user to reload form
+            tools.swal("info", "editGrid.urlChanged", "editGrid.urlChangedMessage");
+        },
+
         initialize : function(options) {
             // set aside url for autocompTree fields by key, will be used by editor
             var wsURL;
