3f33a4fda39dead74b88597d8365c7443eb90693
rkjdid <romainknezevic@gmail.com>
2017-12-08 15:27:27 +0100

    PositionFieldView == ThesaurusFieldView

--- assets/js/editionPageModule/views/fieldViews/PositionFieldView.js
+++ assets/js/editionPageModule/views/fieldViews/PositionFieldView.js
@@ -1,21 +1,5 @@
 define([
-    'jquery',
-    'lodash',
-    'backbone',
-    'editionPageModule/views/fieldViews/BaseView'
-], function($, _, Backbone, BaseView) {
-    return BaseView.extend({
-        initialize : function(options) {
-            // set aside url for autocompTree fields by key, will be used by editor
-            var wsURL;
-            if (options.model) {
-                wsURL = options.model.get("webServiceURL");
-            }
-            if (wsURL) {
-                options.model.defaultNode = wsURL;
-            }
-
-            BaseView.prototype.initialize.apply(this, [options]);
-        }
-    });
+    'editionPageModule/views/fieldViews/ThesaurusFieldView'
+], function(ThesaurusFieldView) {
+    return ThesaurusFieldView;
 });
