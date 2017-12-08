e343951f4239feb25c7fcdcba83b25bf5f3312b8
rkjdid <romainknezevic@gmail.com>
2017-12-08 11:24:50 +0100

    demote AutocompTreeView & TreeView views to BaseViews (for now)

--- assets/js/editionPageModule/views/fieldViews/AutocompleteTreeViewFieldView.js
+++ assets/js/editionPageModule/views/fieldViews/AutocompleteTreeViewFieldView.js
@@ -2,39 +2,7 @@
     'jquery',
     'lodash',
     'backbone',
-    'editionPageModule/views/fieldViews/BaseView',
-    'jquery-ui',
-    'autocompTree'
+    'editionPageModule/views/fieldViews/BaseView'
 ], function($, _, Backbone, BaseView) {
-
-    var AutocompleteTreeViewFieldView = BaseView.extend({
-        initialize : function(options) {
-            var opt = options;
-            BaseView.prototype.initialize.apply(this, [opt]);
-            this.mainChannel = Backbone.Radio.channel('global');
-
-            this.mainChannel.on('nodeSelected' + this.model.get('id'), _.bind(function(data) {
-                var key     = data.node.key,
-                    treeID  = '#treeViewtree'+ this.model.get('id');
-
-                this.model.set('defaultNode', data.node.key);
-                this.model.set('fullpath', data.node.data.fullpath);
-
-                $(treeID).autocompTree("getTree").activateKey(key);
-            }, this));
-        },
-
-        render : function() {
-            BaseView.prototype.render.apply(this, arguments);
-            this.$el.find('#tree' + this.model.get('id')).autocompTree({
-                language    : { hasLanguage: true, lng : this.model.get('language') },
-                wsUrl       : 'ressources/thesaurus',
-                webservices : 'autocompleteTreeView.json',
-                startId     : '0'
-            });
-        }
-    });
-
-	return AutocompleteTreeViewFieldView;
-
+    return BaseView;
 });
--- assets/js/editionPageModule/views/fieldViews/TreeViewFieldView.js
+++ assets/js/editionPageModule/views/fieldViews/TreeViewFieldView.js
@@ -2,28 +2,7 @@ define([
     'jquery',
     'lodash',
     'backbone',
-    'editionPageModule/views/fieldViews/BaseView',
-    'jquery-ui',
-    'autocompTree'
+    'editionPageModule/views/fieldViews/BaseView'
 ], function($, _, Backbone, BaseView) {
-
-    var TreeViewFieldView = BaseView.extend({
-        render : function() {
-            BaseView.prototype.render.apply(this, arguments);
-            $('#treeview' + this.model.get('id')).autocompTree({
-                source: [
-                    {title: "Node 1", key: "1"},
-                    {title: "Folder 2", key: "2", folder: true, children: [
-                        {title: "Node 2.1", key: "3"},
-                        {title: "Node 2.2" + this.model.get('defaultNode'), key: "4"}
-                    ]}
-                ],
-                selectMode : 1,
-                defaultkey : "" + this.model.get('defaultNode')
-            });
-        }
-    });
-
-	return TreeViewFieldView;
-
+	return BaseView;
 });
