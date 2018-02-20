8ec9a9a909d3ff3be7277b94434bf4909f62ffac
romfabbro <romain_fabbro@natural-solutions.eu>
2018-02-20 09:01:42 +0100

    add nbFixCol on form and bind it with is grid checkbox

--- assets/js/editionPageModule/collection/EcoreleveCollection.js
+++ assets/js/editionPageModule/collection/EcoreleveCollection.js
@@ -7,13 +7,27 @@ define([
     'backbone',
     '../models/Fields',
     '../editor/CheckboxEditor',
+    '../editor/NumberEditor',
     'backbone.radio',
     '../../Translater',
     'auth',
     'text!../templates/FieldTemplate.html'
-], function ($, Backbone, Fields, CheckboxEditor, Radio, translater, auth, FieldTemplate) {
+], function ($, Backbone, Fields, CheckboxEditor, NumberEditor, Radio, translater, auth, FieldTemplate) {
 
     var fieldTemplate = _.template(FieldTemplate);
+    var customNumberEditor = NumberEditor.extend({
+        render: function(){
+            NumberEditor.prototype.render.call(this);
+            this.$el.append('<br/>');
+            if(this.form.data.isgrid){
+                this.form.afterRender = function(){
+                    var nbFixedColEditor = this.getEditor('nbFixedCol');
+                    nbFixedColEditor.$el.parent().parent().removeClass('hidden');
+                };
+            }
+            return this;
+        }
+    });
     return {
         schemaExtention: {
             author : {
@@ -25,7 +39,17 @@ define([
                 type        : CheckboxEditor,
                 template    : fieldTemplate,
                 fieldClass  : "checkBoxEditor form-group",
-                title       : translater.getValueFromKey('form.isgrid')
+                title       : translater.getValueFromKey('form.isgrid'),
+                handlers: [function(value, input){
+                    //handlers are called on input change
+                    var nbFixedColEditor = input.form.getEditor('nbFixedCol');
+                    if(input.getValue()){
+                        nbFixedColEditor.$el.parent().parent().removeClass('hidden');
+                    } else{
+                        nbFixedColEditor.$el.parent().parent().addClass('hidden');
+                        nbFixedColEditor.setValue('');
+                    }
+                }]
             },
             ishiddenprotocol : {
                 type        : CheckboxEditor,
@@ -33,13 +57,30 @@ define([
                 fieldClass  : "checkBoxEditor form-group",
                 title       : translater.getValueFromKey('form.ishiddenprotocol')
             },
+            nbFixedCol: {
+                type: customNumberEditor,
+                min: 0,
+                template: fieldTemplate,
+                fieldClass: 'hidden',
+                title: translater.getValueFromKey('schema.nbFixedCol'),
+                validators: [function checkValue(value) {
+                    if (value < 0) {
+                        return {
+                            type: 'Invalid number',
+                            message: translater.getValueFromKey('schema.nbFixedColMinValue')
+                        };
+                    }
+                }]
+            },
         },
 
         getExtractedDatas: function() {return {};},
         getSchemaExtention: function(){
             return this.schemaExtention;
         },
-        initializeExtention: function () {return true;},
+        initializeExtention: function () {
+            return true;
+        },
 
         jsonExtention: function (originalForm) {
             if (originalForm) {
@@ -50,7 +91,8 @@ define([
                 isgrid : "",
                 ishiddenprotocol: "",
                 hideprotocolname: "",
-                defaultforfieldactivity: ""
+                defaultforfieldactivity: "",
+                nbFixedCol: ""
             };
         },
         updateAttributesExtention: function () {return true;}
