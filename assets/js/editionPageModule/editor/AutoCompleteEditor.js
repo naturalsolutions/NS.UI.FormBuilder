e743f2c9c405dfd3804671ff7e9e8a2ef448131c
David Lassagne <david_lassagne@natural-solutions.eu>
2019-09-27 17:45:05 +0800

    Fix for AC editor
    Looks like it was only in a removed branch ...

--- assets/js/editionPageModule/editor/AutoCompleteEditor.js
+++ assets/js/editionPageModule/editor/AutoCompleteEditor.js
@@ -4,6 +4,10 @@ define([
     'backbone-forms'
 ], function($, _, Backbone, AutocompleteTemplate) {
     return Backbone.Form.editors.Text.extend({
+        events: {
+            'change input': 'updateValue'
+        },
+
         initialize: function(options) {
             this.options = options;
             Backbone.Form.editors.Text.prototype.initialize.call(this, options);
@@ -34,7 +38,24 @@ define([
             this.$el.i18n();
             this.setElement(this.$el);
 
+            if (this.value)
+                this.$el.find('input').val(this.value);
+
             return this;
+        },
+
+        updateValue: function(e) {
+            this.setValue(this.$el.find('input').val());
+        },
+
+        getValue: function() {
+            return this.value;
+        },
+
+        setValue: function(value) {
+            this.value = value;
+            this.model.view.setValue(this.options.key, value);
+            this.model.set(this.options.key, value, {trigger: true});
         }
     });
 });
